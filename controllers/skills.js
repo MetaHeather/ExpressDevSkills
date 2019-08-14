f57addd4c581de424744c44e3596d7310c3ebcd2
Heather Etheridge <heathernetheridge@gmail.com>
2019-08-14 08:42:12 -0500

    Added controller/route to render index

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,1 +1,17 @@
+//Requires
+//bring in skills model
+var Skills = require('../models/skill');
 
+
+//export object
+module.exports = {
+    index
+}
+
+
+//funciton to render skills list
+function index(req, res) {
+    res.render('skills/index', {
+      skills: Skills.getAll()
+    });
+  }
--- routes/skills.js
+++ routes/skills.js
@@ -1,17 +1,14 @@
 var express = require('express');
 var router = express.Router();
-//bring in skills model
-var Skills = require('../models/skill');
+//include skills controller
+var skillsCtrl = require('../controllers/skills');
+
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.get('/', function(req, res){
-  res.render('skills/index', {
-    skills: Skills.getAll()
-  });
-});
+router.get('/', skillsCtrl.index);
 
 module.exports = router;
