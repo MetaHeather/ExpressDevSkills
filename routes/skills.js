ac3b095d1b768946c0e4e2dc6a0938e77d6a6ba5
Heather Etheridge <heathernetheridge@gmail.com>
2019-08-14 22:52:52 -0500

    Updated skills index.ejs to show list of skills from skills model

--- routes/skills.js
+++ routes/skills.js
@@ -3,12 +3,7 @@ var router = express.Router();
 //include skills controller
 var skillsCtrl = require('../controllers/skills');
 
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
-
 router.get('/', skillsCtrl.index);
+
 
 module.exports = router;
